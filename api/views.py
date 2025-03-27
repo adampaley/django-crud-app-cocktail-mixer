0ab0b17415256c3dec903da56c141b26703c15c7
Adam Paley <apaley95@gmail.com>
2025-03-27 16:29:39 -0400

    defined home and about views

--- api/views.py
+++ api/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse()
+    return HttpResponse('<h1>Hello ᗢ</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About the Cocktail Mixer</h1>')
